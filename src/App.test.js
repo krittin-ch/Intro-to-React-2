18248a72948e3865aa590d9837dafe82b4812ed4
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-29 13:37:41 +0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {MyApp, Profile} from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Profile />);
+  const linkElement = screen.getByText(/welcome to my app/i);
   expect(linkElement).toBeInTheDocument();
 });
