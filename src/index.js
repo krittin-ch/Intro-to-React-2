2e7b497dc3173ad00b40910f6540b70ecf2d23bd
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-29 13:37:47 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {MyApp, Profile} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Profile />
   </React.StrictMode>
 );
 
