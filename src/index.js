0ecd0abb0d3f3cba0d8cbbcb8fdaa6bc77fc3d57
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-03-01 10:18:43 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {MyApp3} from './App';
+import {Game} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyApp3 />
+    <Game />
   </React.StrictMode>
 );
 
